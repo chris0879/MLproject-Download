a5b8ee7c650ef476e2af242ea5bbe11e26dfe46e
chris0879 <laverdechristian@gmail.com>
2023-06-27 12:09:40 +0200

    Update download_data.py

--- download_data.py
+++ download_data.py
@@ -5,14 +5,14 @@ def download_file(url, destination):
     with open(destination, 'wb') as file:
         file.write(response.content)
 
-def main(file_url, destination_path):
+def main(file_url_p, destination_path_p):
     # Specifica l'URL del file remoto da scaricare
     #file_url = 'https://www.deliziedelparnaso.it/data.csv'
-    file_url = file_url
+    file_url = file_url_p
 
     # Specifica il percorso di destinazione in cui salvare il file scaricato
     #destination_path = '/app/chris_python_download.csv'
-    destination_path = destination_path 
+    destination_path = destination_path_p 
 
     # Esegui il download del file remoto
     download_file(file_url, destination_path)
